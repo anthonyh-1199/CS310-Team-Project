3ccf2c1a338c0292e91c1760b943f8a0774c6ce1
csmit <csmith30615@gmail.com>
2020-03-01 13:05:53 -0500

    Badge.java Initial Edit

---
+++ src/edu/jsu/mcis/tas_sp20/Badge.java
@@ -0,0 +1,27 @@
+package edu.jsu.mcis.tas_sp20;
+
+public class Badge {
+    //Initialize Global Variables
+    private String id, name;
+    
+    //Constructor
+    public Badge(String id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    
+    //Getters
+    public String getId(){
+        return this.id;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    @Override
+    public String toString(){
+        String s = "#" + id + " (" + name + ")";
+        return s;
+    }
+}
