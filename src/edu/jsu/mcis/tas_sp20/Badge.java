5381def79b3ee16cc0c637b8cad42143a6a83912
csmit <csmith30615@gmail.com>
2020-03-09 11:59:50 -0500

    Edited badge class

--- src/edu/jsu/mcis/tas_sp20/Badge.java
+++ src/edu/jsu/mcis/tas_sp20/Badge.java
@@ -1,24 +1,46 @@
 package edu.jsu.mcis.tas_sp20;
 
+/**
+ * This class is used to create a badge object containing the id of the badge
+ * and the name of the badge owner.
+ * @author csmit
+ */
 public class Badge {
     //Initialize Global Variables
     private String id, name;
     
     //Constructor
+    /**
+     * Constructor for Badge class, all badges must have an id and a name
+     * @param id String id of the badge
+     * @param name String name of the badge owner
+     */
     public Badge(String id, String name){
         this.id = id;
         this.name = name;
     }
     
     //Getters
+    /**
+     * Getter for the id of this badge
+     * @return String id of this badge
+     */
     public String getID(){
         return this.id;
     }
     
+    /**
+     * Getter for the name of this badge owner
+     * @return String name of this badge owner
+     */
     public String getName(){
         return this.name;
     }
     
+    /**
+     * Convert this badge to a String in format #id (name)
+     * @return String format of this badge
+     */
     @Override
     public String toString(){
         String s = "#" + id + " (" + name + ")";
