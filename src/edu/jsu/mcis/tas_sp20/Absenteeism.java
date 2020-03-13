640c1d5802823a44e1ae3423723f6c7a6bd5d896
Clayton Collum <ccollum@stu.jsu.edu>
2020-03-13 12:41:54 -0500

    adjusted getBadgeId

--- src/edu/jsu/mcis/tas_sp20/Absenteeism.java
+++ src/edu/jsu/mcis/tas_sp20/Absenteeism.java
@@ -1,17 +1,15 @@
 package edu.jsu.mcis.tas_sp20;
 
-import com.sun.tools.corba.se.idl.constExpr.Times;
-
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.GregorianCalendar;
 
 public class Absenteeism {
-    Badge id;
+    String id;
     long tsLong;
     double percentage;
 
-    Absenteeism (Badge id, long tsLong, double percentage) {
+    Absenteeism (String id, long tsLong, double percentage) {
         this.tsLong = tsLong;
         this.id = id;
 
@@ -19,12 +17,12 @@ public class Absenteeism {
     }
 
     // Setter methods
-    public void setID (Badge id) {this.id = id;}
+    public void setBadgeId (String id) {this.id = id;}
     public void setTimestamp (long tsLong) {this.tsLong = tsLong;}
     public void setPercentage (double percentage) {this.percentage = percentage;}
 
     // Getter methods
-    public Badge getID () {return id;}
+    public String getBadgeId () {return id;}
     public long getTimestampLong () {return tsLong;}
     public double getPercentage () {return percentage;}
     public Timestamp getTimestamp() {return new Timestamp(tsLong);}
