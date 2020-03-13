40ab711e20cbf86ffaf308ec679fd343a20f5ef1
Clayton Collum <ccollum@stu.jsu.edu>
2020-03-13 11:46:51 -0500

    Upadated Absenteeism.java

--- src/edu/jsu/mcis/tas_sp20/Absenteeism.java
+++ src/edu/jsu/mcis/tas_sp20/Absenteeism.java
@@ -1,5 +1,34 @@
 package edu.jsu.mcis.tas_sp20;
 
-public class Absenteeism {
+import java.sql.Timestamp;
+import java.util.GregorianCalendar;
 
+public class Absenteeism {
+    String id;
+    Long payperiod;
+    double percentage;
+
+    Absenteeism (Long payperiod, String id) {
+        this.payperiod = payperiod;
+        this.id = id;
+
+        percentage = 0;
+    }
+
+    // Setter methods
+    public void setID (String id) {this.id = id;}
+    public void setPayperiod (Long payperiod) {this.payperiod = payperiod;}
+    public void setPercentage (double percentage) {this.percentage = percentage;}
+
+    // Getter methods
+    public String getID (String id) {return id;}
+    public Long getPayperiod (Long payperiod) {return payperiod;}
+    public double getPercentage (double percentage) {return percentage;}
+
+    // toString method
+    @Override
+    public String toString() {
+        String s = id + "(Pay Period Starting " + payperiod + "): " + percentage;
+        return s;
+    }
 }
