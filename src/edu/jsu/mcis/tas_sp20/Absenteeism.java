fe9c44448348112ed3ab74f479d5ee17317ddcfe
Clayton Collum <ccollum@stu.jsu.edu>
2020-03-13 12:37:19 -0500

    adjusted Absenteeism the sequel

--- src/edu/jsu/mcis/tas_sp20/Absenteeism.java
+++ src/edu/jsu/mcis/tas_sp20/Absenteeism.java
@@ -1,15 +1,18 @@
 package edu.jsu.mcis.tas_sp20;
 
+import com.sun.tools.corba.se.idl.constExpr.Times;
+
+import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.GregorianCalendar;
 
 public class Absenteeism {
     Badge id;
-    long timestamp;
+    long tsLong;
     double percentage;
 
-    Absenteeism (Badge id, long timestamp, double percentage) {
-        this.timestamp = timestamp;
+    Absenteeism (Badge id, long tsLong, double percentage) {
+        this.tsLong = tsLong;
         this.id = id;
 
         this.percentage = percentage;
@@ -17,18 +20,19 @@ public class Absenteeism {
 
     // Setter methods
     public void setID (Badge id) {this.id = id;}
-    public void setTimestamp (Long timestamp) {this.timestamp = timestamp;}
+    public void setTimestamp (long tsLong) {this.tsLong = tsLong;}
     public void setPercentage (double percentage) {this.percentage = percentage;}
 
     // Getter methods
-    public Badge getID (Badge id) {return id;}
-    public Long getTimestamp (long timestamp) {return timestamp;}
-    public double getPercentage (double percentage) {return percentage;}
+    public Badge getID () {return id;}
+    public long getTimestampLong () {return tsLong;}
+    public double getPercentage () {return percentage;}
+    public Timestamp getTimestamp() {return new Timestamp(tsLong);}
 
     // toString method
     @Override
     public String toString() {
-        String s = id + "(Pay Period Starting " + timestamp + "): " + percentage;
+        String s = id + "(Pay Period Starting " + tsLong + "): " + percentage;
         return s;
     }
 }
